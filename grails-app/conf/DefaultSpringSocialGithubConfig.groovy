083245b1cf13b6e0744570764b90861f9acdc554
neodevelop <neodevelop@gmail.com>
2011-11-15 10:46:15 -0600

    WIP
    -Agregamos dependencias necesarias
    -Creamos la configuración de urls
    -Agregamos configuración de Spring

---
+++ grails-app/conf/DefaultSpringSocialGithubConfig.groovy
@@ -0,0 +1,7 @@
+springsocial {
+    github.page.connect = "/springsocial/github/connect"
+    github.page.connectedHome = "/springSocialGithub/index"
+    github.page.profile = "/springSocialGithub/index"
+    github.page.profiles = "/springsocial/github/profiles"
+    github.page.deniedHome = "/springSocialGithub/login"
+}
